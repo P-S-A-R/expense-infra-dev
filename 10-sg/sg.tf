d58db0c05f9d2e0bdeef9b2e0a4fd2dac1d74456
Satya Avinash Reddy Pamireddy <reddyavinash61@gmail.com>
2025-02-26 00:43:45 +0100

    project

--- 10-sg/sg.tf
+++ 10-sg/sg.tf
@@ -38,3 +38,13 @@ module "bastion_sg" {
     vpc_id = data.aws_ssm_parameter.vpc_id.value
     common_tags = var.common_tags
 }
+
+module "app_alb_sg" {
+    source = "git::https://github.com/P-S-A-R/terraform-aws-securitygroup.git?ref=main"
+    project_name = var.project_name
+    environment = var.environment
+    sg_name = "app-alb"
+    sg_description = "created for backend ALB instance in expense dev"
+    vpc_id = data.aws_ssm_parameter.vpc_id.value
+    common_tags = var.common_tags
+}
---
+++ 50-app-alb/main.tf
@@ -0,0 +1,20 @@
+
+module "alb" {
+  source = "terraform-aws-modules/alb/aws"
+  internal = true
+
+  # expense-dev-app-alb
+  name    = "${var.project_name}-${var.environment}-app-alb"
+  vpc_id  = data.aws_ssm_parameter.vpc_id.value
+  subnets = local.private_subnet_ids
+  create_security_group = false
+  security_groups = [local.app_alb_sg_id]
+
+  
+  tags = merge(
+    var.common_tags,
+    {
+        Name = "${var.project_name}-${var.environment}-app-alb"
+    }
+  )
+}
